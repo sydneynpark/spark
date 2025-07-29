b58c55b23ca91e79026a86dcac534258e49bc77f
sydneynpark <sydneynpark@gmail.com>
2025-07-29 15:02:38 -0500

    Minor code cleanup

--- ContentManagement/UpdatePhotoMetadata/src/img_util.py
+++ ContentManagement/UpdatePhotoMetadata/src/img_util.py
@@ -5,32 +5,6 @@ class ImageUtil:
     def __init__(self):
         pass
 
-    def get_exif_details(self, jpg_stream):
-        # Source: https://stackoverflow.com/a/77539176
-        IFD_CODE_LOOKUP = {i.value: i.name for i in ExifTags.IFD}
-
-        with Image.open(jpg_stream) as img:
-
-            img_exif = img.getexif()
-            print(img_exif)
-
-            for tag_code, value in img_exif.items():
-
-                # if the tag is an IFD block, nest into it
-                if tag_code in IFD_CODE_LOOKUP:
-
-                    ifd_tag_name = IFD_CODE_LOOKUP[tag_code]
-                    print(f"IFD '{ifd_tag_name}' (code {tag_code}):")
-                    ifd_data = img_exif.get_ifd(tag_code).items()
-
-                    for nested_key, nested_value in ifd_data:
-                        nested_tag_name = ExifTags.GPSTAGS.get(nested_key, None) or ExifTags.TAGS.get(nested_key, None) or nested_key
-                        print(f"  {nested_tag_name}: {nested_value}")
-
-                else:
-                    # root-level tag
-                    print(f"{ExifTags.TAGS.get(tag_code)}: {value}")
-
     def get_lightroom_keywords(self, jpg_stream):
         with Image.open(jpg_stream) as img:
             img_xmp = img.getxmp()
--- ContentManagement/UpdatePhotoMetadata/src/lambda_function.py
+++ ContentManagement/UpdatePhotoMetadata/src/lambda_function.py
@@ -9,9 +9,7 @@ img = img_util.ImageUtil()
 
 
 def lambda_handler(event, context):
-    #print("Received event: " + json.dumps(event, indent=2))
 
-    # Get the object from the event and show its content type
     bucket = event['Records'][0]['s3']['bucket']['name']
     key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
     event_type = event['Records'][0]['eventName']
