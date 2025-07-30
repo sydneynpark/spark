c6d09bf8f9c63ca4ef211f441277689451164155
sydneynpark <sydneynpark@gmail.com>
2025-07-30 11:41:13 -0500

    Call it from a Lambda function

---
+++ ContentManagement/UpdateBlogPostMetadata/src/aws_util.py
@@ -0,0 +1,9 @@
+import boto3
+
+class AWSUtil:
+
+    def __init__(self):
+        self.s3 = boto3.client('s3')
+    
+    def get_s3_object(self, bucket, key):
+        return self.s3.get_object(Bucket=bucket, Key=key)
